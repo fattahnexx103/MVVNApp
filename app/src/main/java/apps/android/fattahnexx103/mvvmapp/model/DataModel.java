1009dc6f44c179ec1edd971f5ce88f0f3077784c
fattahnexx103 <nexxfattah@gmail.com>
2019-08-29 21:06:50 -0400

    Added dependencies and created model class

---
+++ app/src/main/java/apps/android/fattahnexx103/mvvmapp/model/DataModel.java
@@ -0,0 +1,27 @@
+package apps.android.fattahnexx103.mvvmapp.model;
+
+//Model Class
+public class DataModel {
+
+    private String title:
+    private String description;
+    private String picUrl;
+
+    public DataModel(String title, String description, String picUrl) {
+        this.title = title;
+        this.description = description;
+        this.picUrl = picUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+}
---
+++ app/src/main/java/apps/android/fattahnexx103/mvvmapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package apps.android.fattahnexx103.mvvmapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import apps.android.fattahnexx103.mvvmapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
